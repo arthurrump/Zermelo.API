f187c09b27b2d0ec075dcbb75e0aef9459df83f8
Arthur Rump <arthurrump@outlook.com>
2016-05-03 19:25:53 +0200

    [API] Add Announcement model

---
+++ Zermelo/Zermelo.API/Models/Announcement.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zermelo.API.Helpers;
+
+namespace Zermelo.API.Models
+{
+    /// <summary>
+    /// Model for Announcements, as described at https://zermelo.atlassian.net/wiki/display/DEV/Announcement
+    /// </summary>
+    public class Announcement
+    {
+        /// <summary>
+        /// The id of the announcement
+        /// </summary>
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        /// <summary>
+        /// The date from which the announcement should be shown
+        /// </summary>
+        [JsonProperty("start")]
+        [JsonConverter(typeof(UnixTimeToDateTimeOffsetJsonConverter))]
+        public DateTimeOffset Start { get; set; }
+
+        /// <summary>
+        /// The date until which the announcement should be shown
+        /// </summary>
+        [JsonProperty("end")]
+        [JsonConverter(typeof(UnixTimeToDateTimeOffsetJsonConverter))]
+        public DateTimeOffset End { get; set; }
+
+        /// <summary>
+        /// The title of the announcement
+        /// </summary>
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The detail text of the announcement
+        /// </summary>
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+}
