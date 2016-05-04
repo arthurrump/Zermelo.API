0ff66b04a0c4266fea63774fb5cf7245097b0714
Arthur Rump <arthurrump@outlook.com>
2016-05-04 16:21:52 +0200

    [API] Add User model

---
+++ Zermelo/Zermelo.API/Models/User.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zermelo.API.Models
+{
+    /// <summary>
+    /// Model for Users, as described at https://zermelo.atlassian.net/wiki/display/DEV/User
+    /// </summary>
+    class User
+    {
+        /// <summary>
+        /// The identifier for a user. Usually a student id or abbreviation.
+        /// </summary>
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        /// <summary>
+        /// The roles that have been assigned to this user.
+        /// </summary>
+        [JsonProperty("roles")]
+        public IEnumerable<string> Roles { get; set; }
+
+        /// <summary>
+        /// The first name of a user.
+        /// </summary>
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Dutch: "tussenvoegsel"
+        /// </summary>
+        [JsonProperty("prefix")]
+        public string Prefix { get; set; }
+
+        /// <summary>
+        /// The last name of the user.
+        /// </summary>
+        [JsonProperty("lastName")]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The full name of the user.
+        /// </summary>
+        public string FullName
+            => $"{FirstName} {Prefix} {LastName}";
+    }
+}
