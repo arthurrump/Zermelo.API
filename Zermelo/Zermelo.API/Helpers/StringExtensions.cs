a3d442cd598abba01e15ea9d283e4e39a48443ab
Arthur Rump <arthurrump@outlook.com>
2016-05-04 19:53:35 +0200

    [API] Add helper to remove all occurences of a char in a string

---
+++ Zermelo/Zermelo.API/Helpers/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zermelo.API.Helpers
+{
+    static class StringExtensions
+    {
+        public static string RemoveAll(this string s, char character)
+        {
+            while (s.IndexOf(character) != -1)
+                s = s.Remove(s.IndexOf(character), 1);
+            return s;
+        }
+    }
+}
