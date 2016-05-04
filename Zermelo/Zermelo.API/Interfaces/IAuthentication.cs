373ab7969357fca4679352c158c3670fb04c7b5c
Arthur Rump <arthurrump@outlook.com>
2016-05-04 17:53:52 +0200

    [API] Add IAuthentication interface

---
+++ Zermelo/Zermelo.API/Interfaces/IAuthentication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zermelo.API.Interfaces
+{
+    internal interface IAuthentication
+    {
+        string Host { get; }
+        string Token { get; }
+    }
+}
