a2e40ade0897c473a9b174907553175768b6ce2c
Arthur Rump <arthurrump@outlook.com>
2016-05-05 20:53:52 +0200

    [API] Implement Appointment

--- Zermelo/Zermelo.API/Endpoints/AppointmentsEndpoint.cs
+++ Zermelo/Zermelo.API/Endpoints/AppointmentsEndpoint.cs
@@ -1,19 +1,157 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Zermelo.API.Exceptions;
+using Zermelo.API.Helpers;
 using Zermelo.API.Interfaces;
+using Zermelo.API.Models;
 using Zermelo.API.Services.Interfaces;
 
 namespace Zermelo.API.Endpoints
 {
     class AppointmentsEndpoint : EndpointBase
     {
+        private const string _endpoint = "appointments";
+
         internal AppointmentsEndpoint(IAuthentication auth, IUrlBuilder urlBuilder, IHttpService httpService, IJsonService jsonService)
             : base(auth, urlBuilder, httpService, jsonService)
         {
+        }
 
+        /// <summary>
+        /// Get all appointments that start between the specified start and end points.
+        /// </summary>
+        /// <param name="start">The date from which to get appointments.</param>
+        /// <param name="end">The date until which to get appointments.</param>
+        /// <param name="user">The user to get appointments for. Defaults to <c>"~me"</c> to get appointments for the current user.</param>
+        /// <param name="fields">
+        /// The fields (in original json names) to get. Defaults to <c>null</c>, which gets the server defaults.
+        /// An empty list will also result in the server defaults.
+        /// </param>
+        /// <returns>List of appointments.</returns>
+        public async Task<IEnumerable<Appointment>> GetByDateAsync(DateTimeOffset start, DateTimeOffset end, string user = "~me", List<string> fields = null)
+        {
+            if (start <= end)
+                throw new ArgumentOutOfRangeException(nameof(end), end,
+                    $"The value of the {nameof(end)} parameter should be later in time " + 
+                    $"than the value of the {nameof(start)} parameter (value: {start.ToString()}).");
+
+            Dictionary<string, string> urlOptions = new Dictionary<string, string>
+            {
+                { "user", user },
+                { "start", UnixTimeHelpers.ToUnixTimeSeconds(start.ToUniversalTime()).ToString() },
+                { "end", UnixTimeHelpers.ToUnixTimeSeconds(end.ToUniversalTime()).ToString() }
+            };
+
+            if (fields != null && fields.Any())
+                urlOptions.Add("fields", fields.ToCommaSeperatedString());
+
+            string url = _urlBuilder.GetAuthenticatedUrl(_auth, _endpoint, urlOptions);
+
+            IHttpResponse httpResponse = await _httpService.GetAsync(url);
+
+            if (httpResponse.StatusCode != 200)
+                throw new ZermeloHttpException(httpResponse);
+
+            string json = httpResponse.Response;
+
+            return _jsonService.DeserializeCollection<Appointment>(json);
+        }
+
+        /// <summary>
+        /// Get a specific appointment by it's id.
+        /// </summary>
+        /// <param name="id">The id of the appointment to get.</param>
+        /// <param name="fields">
+        /// The fields (in original json names) to get. Defaults to <c>null</c>, which gets the server defaults.
+        /// An empty list will also result in the server defaults.
+        /// </param>
+        /// <returns>The requested appointment, or, if it's not found, null.</returns>
+        public async Task<Appointment> GetSingleByIdAsync(long id, List<string> fields = null)
+        {
+            Dictionary<string, string> urlOptions = new Dictionary<string, string>
+            {
+                { "id", id.ToString() }
+            };
+
+            if (fields != null && fields.Any())
+                urlOptions.Add("fields", fields.ToCommaSeperatedString());
+
+            string url = _urlBuilder.GetAuthenticatedUrl(_auth, _endpoint, urlOptions);
+
+            IHttpResponse httpResponse = await _httpService.GetAsync(url);
+
+            if (httpResponse.StatusCode != 200)
+                throw new ZermeloHttpException(httpResponse);
+
+            string json = httpResponse.Response;
+
+            IEnumerable<Appointment> result = _jsonService.DeserializeCollection<Appointment>(json);
+
+            if (result.Count() < 1)
+                return null;
+
+            return result.SingleOrDefault();
+        }
+
+        /// <summary>
+        /// Get all versions of an appointment instance by it's <c>InstanceId</c>.
+        /// </summary>
+        /// <param name="instanceId">The id of the appointment instance.</param>
+        /// <param name="fields">
+        /// The fields (in original json names) to get. Defaults to <c>null</c>, which gets the server defaults.
+        /// An empty list will also result in the server defaults.
+        /// </param>
+        /// <returns>All versions of an appointment instance.</returns>
+        public async Task<IEnumerable<Appointment>> GetAllVersionsByInstanceIdAsync(long instanceId, List<string> fields = null)
+        {
+            Dictionary<string, string> urlOptions = new Dictionary<string, string>
+            {
+                { "appointmentInstance", instanceId.ToString() }
+            };
+
+            if (fields != null && fields.Any())
+                urlOptions.Add("fields", fields.ToCommaSeperatedString());
+
+            string url = _urlBuilder.GetAuthenticatedUrl(_auth, _endpoint, urlOptions);
+
+            IHttpResponse httpResponse = await _httpService.GetAsync(url);
+
+            if (httpResponse.StatusCode != 200)
+                throw new ZermeloHttpException(httpResponse);
+
+            string json = httpResponse.Response;
+
+            return _jsonService.DeserializeCollection<Appointment>(json);
+        }
+
+        /// <summary>
+        /// Get appointments by a custom query.
+        /// </summary>
+        /// <param name="urlOptions">The options you want to be in the url.</param>
+        /// <param name="fields">
+        /// The fields (in original json names) to get. Defaults to <c>null</c>, which gets the server defaults.
+        /// An empty list will also result in the server defaults.
+        /// </param>
+        /// <returns>The requested appointments.</returns>
+        public async Task<IEnumerable<Appointment>> GetByCustomUrlOptionsAsync(Dictionary<string, string> urlOptions, List<string> fields = null)
+        {
+            if (fields != null && fields.Any())
+                urlOptions.Add("fields", fields.ToCommaSeperatedString());
+
+            string url = _urlBuilder.GetAuthenticatedUrl(_auth, _endpoint, urlOptions);
+
+            IHttpResponse httpResponse = await _httpService.GetAsync(url);
+
+            if (httpResponse.StatusCode != 200)
+                throw new ZermeloHttpException(httpResponse);
+
+            string json = httpResponse.Response;
+
+            return _jsonService.DeserializeCollection<Appointment>(json);
         }
     }
 }
