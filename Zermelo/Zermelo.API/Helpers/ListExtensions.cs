e0080f7eb60bd0165002e2173c8fb986755aa06c
Arthur Rump <arthurrump@outlook.com>
2016-05-05 18:50:10 +0200

    [API] Add ListExtensions.ToCommaSeperatedList() helper

---
+++ Zermelo/Zermelo.API/Helpers/ListExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zermelo.API.Helpers
+{
+    internal static class ListExtensions
+    {
+        public static string ToCommaSeperatedString<T>(this List<T> list)
+        {
+            string s = "";
+
+            foreach (T t in list)
+            {
+                s += t.ToString();
+                s += ",";
+            }
+
+            s = s.TrimEnd(',');
+
+            return s;
+        }
+    }
+}
