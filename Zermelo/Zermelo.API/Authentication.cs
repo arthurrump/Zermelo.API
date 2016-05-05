755974289c637d2426289aea63315c98ea9fe285
Arthur Rump <arthurrump@outlook.com>
2016-05-05 15:32:40 +0200

    [API] Add Authentication and AuthenticationFactory

---
+++ Zermelo/Zermelo.API/Authentication.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zermelo.API.Interfaces;
+using Zermelo.API.Helpers;
+using Zermelo.API.Services.Interfaces;
+
+namespace Zermelo.API
+{
+    public class Authentication : IAuthentication
+    {
+        internal Authentication(string host, string token)
+        {
+            this.Host = host;
+            this.Token = token;
+        }
+
+        public string Host { get; }
+        public string Token { get; }
+    }
+}
---
+++ Zermelo/Zermelo.API/Factories/AuthenticationFactory.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zermelo.API.Helpers;
+using Zermelo.API.Interfaces;
+using Zermelo.API.Services.Interfaces;
+
+namespace Zermelo.API.Factories
+{
+    class AuthenticationFactory
+    {
+        private IUrlBuilder _urlBuilder;
+        private IHttpService _httpService;
+        private IJsonService _jsonService;
+
+        internal AuthenticationFactory(IUrlBuilder urlBuilder, IHttpService httpService, IJsonService jsonService)
+        {
+            _urlBuilder = urlBuilder;
+            _httpService = httpService;
+            _jsonService = jsonService;
+        }
+
+        public async Task<IAuthentication> WithCode(string host, string code)
+        {
+            const string _endpoint = "oauth/token";
+
+            code = code.RemoveAll(' ');
+            host = host.Trim().ToLower();
+
+            Dictionary<string, string> urlOptions = new Dictionary<string, string>
+            {
+                { "grant_type", "authorization_code" },
+                { "code", code }
+            };
+
+            string url = _urlBuilder.GetUrl(host, _endpoint, urlOptions);
+
+            IHttpResponse httpResponse = await _httpService.PostAsync(url, "");
+
+            if (httpResponse.StatusCode != 200)
+                throw new Exception($"{{ \"ErrorType\": \"HTTP\", \"HttpStatus\": \"{httpResponse.Status}\", \"HttpCode\": {httpResponse.StatusCode} }}");
+
+            string json = httpResponse.Response;
+            string token = _jsonService.GetValue<string>(json, "access_token");
+
+            return new Authentication(host, token);
+        }
+
+        public IAuthentication WithToken(string host, string token)
+        {
+            return new Authentication(host, token);
+        }
+    }
+}
