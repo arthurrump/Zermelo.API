d79a3886a5819d56516fb05282dd0d11d713ad88
Arthur Rump <arthurrump@outlook.com>
2016-05-06 18:34:02 +0200

    [API] Add AppointmentsEndpointTests
    1 test -> checks if exception thrown when start is later than end

---
+++ Zermelo/Zermelo.API.Tests/Endpoints/AppointmentsEndpointTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Zermelo.API.Endpoints;
+using Zermelo.API.Services;
+
+namespace Zermelo.API.Tests.Endpoints
+{
+    public class AppointmentsEndpointTests
+    {
+        [Fact]
+        public static async void ShouldThrowExceptionWhenIncorrectDate()
+        {
+            var sut = new AppointmentsEndpoint(
+                new Authentication("", ""),
+                new UrlBuilder(),
+                new HttpService(),
+                new JsonService());
+
+            DateTimeOffset end = new DateTimeOffset(2016, 5, 6, 17, 05, 42, TimeSpan.Zero);
+            DateTimeOffset start = end.AddDays(7);
+
+            ArgumentOutOfRangeException ex = await Assert.ThrowsAsync<ArgumentOutOfRangeException>(() => 
+                sut.GetByDateAsync(start, end));
+
+            Assert.Equal("end", ex.ParamName);
+        }
+    }
+}
