dab0f3fb9a0437dcdcc7fcd85a7c85c72250ef38
buildguest <buildguest>
2016-05-21 19:52:38 +0000

    4: Test

---
+++ data/Navigation.js
@@ -0,0 +1,137 @@
+
+
+var sharpDox = sharpDox || {};
+
+sharpDox.navigationData = [
+			
+				{
+					id: '2f85f025-21c1-46ec-b36c-bf58cf7fee20',
+					text: 'Zermelo.API',
+					icon: 'icon-folder-close',
+					children: [
+												
+						
+
+	{
+		id: 'Zermelo.API',
+		text: 'Zermelo.API',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#/namespace/Zermelo.API' },
+		children: [
+			
+ 
+
+		{
+			id: 'Zermelo.API.Authentication',
+			text: 'Authentication',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/Zermelo.API.Authentication' }
+        },	
+		{
+			id: 'Zermelo.API.ZermeloAuthenticator',
+			text: 'ZermeloAuthenticator',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/Zermelo.API.ZermeloAuthenticator' }
+        },	
+		{
+			id: 'Zermelo.API.ZermeloConnection',
+			text: 'ZermeloConnection',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/Zermelo.API.ZermeloConnection' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'Zermelo.API.Endpoints',
+		text: 'Zermelo.API.Endpoints',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#/namespace/Zermelo.API.Endpoints' },
+		children: [
+			
+ 
+
+		{
+			id: 'Zermelo.API.Endpoints.AnnouncementsEndpoint',
+			text: 'AnnouncementsEndpoint',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/Zermelo.API.Endpoints.AnnouncementsEndpoint' }
+        },	
+		{
+			id: 'Zermelo.API.Endpoints.AppointmentsEndpoint',
+			text: 'AppointmentsEndpoint',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/Zermelo.API.Endpoints.AppointmentsEndpoint' }
+        },	
+		{
+			id: 'Zermelo.API.Endpoints.AuthenticationEndpoint',
+			text: 'AuthenticationEndpoint',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/Zermelo.API.Endpoints.AuthenticationEndpoint' }
+        },	
+		{
+			id: 'Zermelo.API.Endpoints.UsersEndpoint',
+			text: 'UsersEndpoint',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/Zermelo.API.Endpoints.UsersEndpoint' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'Zermelo.API.Exceptions',
+		text: 'Zermelo.API.Exceptions',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#/namespace/Zermelo.API.Exceptions' },
+		children: [
+			
+ 
+
+		{
+			id: 'Zermelo.API.Exceptions.ZermeloHttpException',
+			text: 'ZermeloHttpException',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/Zermelo.API.Exceptions.ZermeloHttpException' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'Zermelo.API.Models',
+		text: 'Zermelo.API.Models',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#/namespace/Zermelo.API.Models' },
+		children: [
+			
+ 
+
+		{
+			id: 'Zermelo.API.Models.Announcement',
+			text: 'Announcement',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/Zermelo.API.Models.Announcement' }
+        },	
+		{
+			id: 'Zermelo.API.Models.Appointment',
+			text: 'Appointment',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/Zermelo.API.Models.Appointment' }
+        },	
+		{
+			id: 'Zermelo.API.Models.User',
+			text: 'User',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/Zermelo.API.Models.User' }
+        },	
+
+
+		]
+	},
+
+
+					]
+				}
+			];
