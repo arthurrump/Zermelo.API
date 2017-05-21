2a26956016e49e95fa3076843ddc8b0cee0a4cf6
Arthur Rump <arthurrump@outlook.com>
2017-05-21 22:12:21 +0200

    Fix docs, really revert to d086133

---
+++ data/Navigation.js
@@ -0,0 +1,150 @@
+
+
+var sharpDox = sharpDox || {};
+
+sharpDox.navigationData = [
+							
+				
+
+{
+	id: 'getting-started',
+	text: 'Getting started',
+		icon: 'icon-file-text',
+	a_attr: { href : '#/article/getting-started' },
+		children: [
+			]
+},
+
+
+					
+				{
+					id: 'bafdd7b7-8ed5-4ff1-a683-9fd0cfec0563',
+					text: 'Zermelo.API',
+					icon: 'icon-folder-close',
+					children: [
+												
+						
+
+	{
+		id: 'Zermelo.API',
+		text: 'Zermelo.API',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#/namespace/Zermelo.API' },
+		children: [
+			
+ 
+
+		{
+			id: 'Zermelo.API.Authentication',
+			text: 'Authentication',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/Zermelo.API.Authentication' }
+        },	
+		{
+			id: 'Zermelo.API.ZermeloAuthenticator',
+			text: 'ZermeloAuthenticator',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/Zermelo.API.ZermeloAuthenticator' }
+        },	
+		{
+			id: 'Zermelo.API.ZermeloConnection',
+			text: 'ZermeloConnection',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/Zermelo.API.ZermeloConnection' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'Zermelo.API.Endpoints',
+		text: 'Zermelo.API.Endpoints',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#/namespace/Zermelo.API.Endpoints' },
+		children: [
+			
+ 
+
+		{
+			id: 'Zermelo.API.Endpoints.AnnouncementsEndpoint',
+			text: 'AnnouncementsEndpoint',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/Zermelo.API.Endpoints.AnnouncementsEndpoint' }
+        },	
+		{
+			id: 'Zermelo.API.Endpoints.AppointmentsEndpoint',
+			text: 'AppointmentsEndpoint',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/Zermelo.API.Endpoints.AppointmentsEndpoint' }
+        },	
+		{
+			id: 'Zermelo.API.Endpoints.AuthenticationEndpoint',
+			text: 'AuthenticationEndpoint',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/Zermelo.API.Endpoints.AuthenticationEndpoint' }
+        },	
+		{
+			id: 'Zermelo.API.Endpoints.UsersEndpoint',
+			text: 'UsersEndpoint',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/Zermelo.API.Endpoints.UsersEndpoint' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'Zermelo.API.Exceptions',
+		text: 'Zermelo.API.Exceptions',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#/namespace/Zermelo.API.Exceptions' },
+		children: [
+			
+ 
+
+		{
+			id: 'Zermelo.API.Exceptions.ZermeloHttpException',
+			text: 'ZermeloHttpException',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/Zermelo.API.Exceptions.ZermeloHttpException' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'Zermelo.API.Models',
+		text: 'Zermelo.API.Models',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#/namespace/Zermelo.API.Models' },
+		children: [
+			
+ 
+
+		{
+			id: 'Zermelo.API.Models.Announcement',
+			text: 'Announcement',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/Zermelo.API.Models.Announcement' }
+        },	
+		{
+			id: 'Zermelo.API.Models.Appointment',
+			text: 'Appointment',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/Zermelo.API.Models.Appointment' }
+        },	
+		{
+			id: 'Zermelo.API.Models.User',
+			text: 'User',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/Zermelo.API.Models.User' }
+        },	
+
+
+		]
+	},
+
+
+					]
+				}
+			];
---
+++ data/Project.js
@@ -0,0 +1,12 @@
+
+
+var sharpDox = sharpDox || {};
+
+sharpDox.projectData = {
+  name: 'Zermelo.API - Connect to Zermelo from your .NET app',
+       author: 'Arthur Rump',    authorHomepage: 'http://arthurrump.azurewebsites.net',      footerLine: '<p>© 2016 Arthur Rump</p>',   hasLogo: true,
+  targetFxs: [
+			"Portable Class Libraries",
+			"Unknown",
+	  ]
+}
