8fc029bcac596f59729787f7c3555ff76831ce3c
Arthur Rump <arthurrump@outlook.com>
2017-06-10 20:54:51 +0200

    Update version patching to use MSBuild
    As suggested on my blog: http://disq.us/p/1j8rpnx

--- scripts/patch-version.ps1
+++
@@ -1,4 +0,0 @@
-$path = (Get-Item .\Zermelo.API\Zermelo.API.csproj).FullName
-$csproj = [xml](Get-Content $path)
-$csproj.Project.PropertyGroup.Version = $Env:APPVEYOR_BUILD_VERSION
-$csproj.Save($path)
